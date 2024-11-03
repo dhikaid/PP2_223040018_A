430fee9b407d58bb2bc7e81f285ab48b7b2c52c8
DhikaID <bhadrika.223040018@mail.unpas.ac.id>
2024-11-03 19:09:38 +0700

    pertemuan 7

---
+++ P7/Membership/src/db/MysqlConnection.java
@@ -0,0 +1,31 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MysqlConnection {
+    private final static String DB_URL = "jbdc:mysql://localhost:3306/pp2_membership";
+    private final static String DB_USER = "root";
+    private final static String DB_PASS = "";
+    
+    private static MysqlConnection instance;
+
+    public static MysqlConnection getInstance(){
+        if(instance == null){
+            instance = new MysqlConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection(){
+        Connection connection = null;
+
+        try {
+            Class.forName("com.mysql.cj.jbdc.Driver");
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
